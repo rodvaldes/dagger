e690790e14a713d62e9f9db719d435a92f9a0a52
Bitbucket Admin <rodrigovaldes@gmail.com>
2022-12-25 17:55:52 -0300

    Configuración multiples os y archs

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
     "context"
     "fmt"
-    
+    "os"
 
     "dagger.io/dagger"
 )
@@ -17,6 +17,10 @@ func main() {
 func build(ctx context.Context) error {
     fmt.Println("Construyendo con Dagger")
 
+    // Define una matriz de build
+    oses := []string{"linux", "darwin"}
+    arches := []string{"amd64", "arm64"}
+
     // Inicializa el cliente Dagger
     client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
     if err != nil {
@@ -27,21 +31,35 @@ func build(ctx context.Context) error {
     // Obtiene la referencia al proyecto local
     src := client.Host().Directory(".")
 
+    // Crear un directorio vacio para almacenar las salidas de los builds
+    outputs := client.Directory()
+
     // get `golang` image
     golang := client.Container().From("golang:latest")
 
     // Montar el repositorio clonado en la imagen `golang`
     golang = golang.WithMountedDirectory("/src", src).WithWorkdir("/src")
 
-    // Define el comando de build de aplicación
-    path := "build/"
-    golang = golang.WithExec([]string{"go", "build", "-o", path})
+    for _, goos := range oses {
+        for _, goarch := range arches {
+            // Crear un directorio para cada os y arquitectura
+            path := fmt.Sprintf("build/%s/%s/", goos, goarch)
 
-    // Obtienen la referencia del directorio de build en el contenedor
-    output := golang.Directory(path)
+            // Configurar GOARCH y GOOS en el ambiente de construcción
+            build := golang.WithEnvVariable("GOOS", goos)
+            build = build.WithEnvVariable("GOARCH", goarch)
 
-    // Escribe el contenido del directorio build/ al host
-    _, err = output.Export(ctx, path)
+            // construir la aplicación
+            build = build.WithExec([]string{"go", "build", "-o", path})
+
+            // get reference to build output directory in container
+            outputs = outputs.WithDirectory(path, build.Directory(path))
+        }
+    }
+    _, err = outputs.Export(ctx, ".")
+    if err != nil {
+        return err
+    }
 
     return nil
 }
