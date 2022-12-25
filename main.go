2eac7f6044b61fbd6b6dcf063faff9c9f3bcc990
Bitbucket Admin <rodrigovaldes@gmail.com>
2022-12-25 18:03:52 -0300

    Agrega configuraciín multi version

--- main.go
+++ main.go
@@ -15,51 +15,53 @@ func main() {
 }
 
 func build(ctx context.Context) error {
-    fmt.Println("Construyendo con Dagger")
+    fmt.Println("Building with Dagger")
 
-    // Define una matriz de build
+    // define build matrix
     oses := []string{"linux", "darwin"}
     arches := []string{"amd64", "arm64"}
+    goVersions := []string{"1.18", "1.19"}
 
-    // Inicializa el cliente Dagger
+    // initialize Dagger client
     client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
     if err != nil {
         return err
     }
     defer client.Close()
 
-    // Obtiene la referencia al proyecto local
+    // get reference to the local project
     src := client.Host().Directory(".")
 
-    // Crear un directorio vacio para almacenar las salidas de los builds
+    // create empty directory to put build outputs
     outputs := client.Directory()
 
-    // get `golang` image
-    golang := client.Container().From("golang:latest")
+    for _, version := range goVersions {
+        // get `golang` image for specified Go version
+        imageTag := fmt.Sprintf("golang:%s", version)
+        golang := client.Container().From(imageTag)
+        // mount cloned repository into `golang` image
+        golang = golang.WithMountedDirectory("/src", src).WithWorkdir("/src")
 
-    // Montar el repositorio clonado en la imagen `golang`
-    golang = golang.WithMountedDirectory("/src", src).WithWorkdir("/src")
+        for _, goos := range oses {
+            for _, goarch := range arches {
+                // create a directory for each os, arch and version
+                path := fmt.Sprintf("build/%s/%s/%s/", version, goos, goarch)
+                // set GOARCH and GOOS in the build environment
+                build := golang.WithEnvVariable("GOOS", goos)
+                build = build.WithEnvVariable("GOARCH", goarch)
 
-    for _, goos := range oses {
-        for _, goarch := range arches {
-            // Crear un directorio para cada os y arquitectura
-            path := fmt.Sprintf("build/%s/%s/", goos, goarch)
+                // build application
+                build = build.WithExec([]string{"go", "build", "-o", path})
 
-            // Configurar GOARCH y GOOS en el ambiente de construcción
-            build := golang.WithEnvVariable("GOOS", goos)
-            build = build.WithEnvVariable("GOARCH", goarch)
-
-            // construir la aplicación
-            build = build.WithExec([]string{"go", "build", "-o", path})
-
-            // get reference to build output directory in container
-            outputs = outputs.WithDirectory(path, build.Directory(path))
+                // get reference to build output directory in container
+                outputs = outputs.WithDirectory(path, build.Directory(path))
+            }
         }
     }
+    // write build artifacts to host
     _, err = outputs.Export(ctx, ".")
     if err != nil {
         return err
     }
-
     return nil
 }
